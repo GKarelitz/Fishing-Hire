1e16e0e05fe1837bde5a467fa846119c1b988371
GKarelitz <Gkarelitz@gmail.com>
2015-03-06 23:31:59 -0500

    UI + Button + Functions

--- function.js
+++ function.js
@@ -1,13 +1,26 @@
+var location, date, people, type, time;
+
 $(document).ready(function(){
-
-	// DATEPICKER FUNCTION \\
-	$('.input-group.date').datepicker({
-    	autoclose: true
-	});
-    
-
-
 
 });
 
+// DATEPICKER FUNCTION //
+$('.input-group.date').datepicker({
+    	autoclose: true
+});
 
+// ON MAIN BUTTON CLICK // 
+$( "#mainbtn" ).bind( "click", function(event, ui) {
+  		getInput();
+});
+
+
+// GET INPUT INFORMATION //
+function getInput() {
+
+	var location = $( "#wherefield" ).val();
+	var date = $( "#datefield" ).val();
+	var people = $( "#peoplefield" ).val();
+	var type = $( "#typefield" ).val();
+
+}
